74cfe3bd38306dea0e401fec0d7897e79e5021c3
christhirst <chrisgluon@gmail.com>
2018-09-24 08:09:35 +0200

    Update filter.sh

--- filter.sh
+++ filter.sh
@@ -1,25 +1,42 @@
-#!/usr/bin/python
-
 import re
 import os
+import fileinput
+
+#character x to y times: {x,y}
+#any character: .
+
+####Directory to search COMPLETELY
+searchDir = "/wls2"
+#### Patter to search for/and replace
+regex = re.compile(r'.{0,20}bund.de', re.IGNORECASE)
+#### True for replacing ####
+replace = False
+
+
 
 dirname = os.path.dirname(os.path.realpath(__file__))
-regex = re.compile(r'\b\w{4}\b')
 #print(regex)
-  #      read_data = g.read()
-for subdir, dirs, files in os.walk(dirname+"/g"):
-    for file in files:
-        #print os.path.join(subdir, file)
-        filepath = subdir + os.sep + file
-        #file = open(filepath, "r")
-        with open(filepath, "r") as file:
-          gg = file.read()
-          print(gg)         
-          print(len(re.findall(regex, gg)))
 
-          #for line in file:
-            #four_letter_words = re.findall(regex, line)
-            #for word in four_letter_words:
-              #print(word)
-          #print(line)
-        #p = re.sub(read_data ,read_data)
+sum = 0
+for subdir, dirs, files in os.walk(dirname+searchDir):
+    for file in files:
+        path =  os.path.join(subdir, file)
+        filename = subdir + os.sep + file
+        with open(filename, "r+") as file:
+          readFile = file.read()
+          sum+=len(re.findall(regex, readFile))
+          #print(readFile)
+          item = (re.findall(regex, readFile))
+          readFile = re.sub(regex, "HOST", readFile)
+
+          if replace:
+            fileout = open(path, 'w')
+            fileout.write(readFile)
+            fileout.close()
+
+          if (item):
+            print(item)
+            #print(readFile)
+
+print(sum)
+~
